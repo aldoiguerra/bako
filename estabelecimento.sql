8266b4477c3110a462510fbf44ebb99bebcfb49f
Aldo <Aldo@aig-note>
2013-10-15 22:00:13 -0300

    Alteração da persistencia para incluir os campos de status

--- estabelecimento.sql
+++ estabelecimento.sql
@@ -1,27 +1,25 @@
 start transaction;
 
-drop database if exists dbestabelecimento;
-create database dbestabelecimento;
-
-use dbestabelecimento;
-
 create table usuario(
 	usuario varchar(20) not null,
         senha varchar(50) not null,
         nome varchar(50) not null,
         tipo int not null,
+        status tinyint not null,
 	primary key (usuario)
 )engine = InnoDB;
 
 create table adicional(
 	id integer not null auto_increment,
 	descricao varchar(50) not null,
+        status tinyint not null,
 	primary key (id)
 )engine = InnoDB;
 
 create table categoria(
 	id integer not null auto_increment,
 	descricao varchar(50) not null,
+        status tinyint not null,
         categoriaPaiId integer,
 	primary key (id)
 )engine = InnoDB;
@@ -43,7 +41,8 @@ create table produto(
 	nome varchar(50) not null,
 	descricao varchar(250) not null,
 	preco float(15,2) not null,
-        foto varchar(100) not null,
+        foto varchar(200),
+        status tinyint not null,
         categoriaId integer,
 	primary key (id),
         foreign key (categoriaId) references categoria(id)
@@ -109,15 +108,6 @@ CREATE FUNCTION buscarDescricao(idPai INT)
     END//
 DELIMITER ;
 
---S%s@dm1n
-insert into usuario (nome,usuario,senha,tipo) values ('Administrador','sysadmin','742379261b4ba6149a2c3bc7ca8d1cb31f176642',1);
-
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 2','user2','12345',2);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 3','user3','12345',3);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 4','user4','12345',1);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 5','user5','12345',2);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 6','user6','12345',3);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 7','user7','12345',1);
---insert into usuario (nome,usuario,senha,tipo) values ('Usuário 8','user8','12345',2);
+insert into usuario (nome,usuario,senha,tipo,status) values ('Administrador','sysadmin','742379261b4ba6149a2c3bc7ca8d1cb31f176642',1,1);
 
 commit;
